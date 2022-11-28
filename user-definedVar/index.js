67658de229dedd327e9ff22e03c108f6a305f0f1
EmmanuelU <emmanuelurias60@icloud.com>
2022-11-28 12:13:49 -0800

    Added practice files

---
+++ user-definedVar/index.js
@@ -0,0 +1,4 @@
+var varName = 'brian';
+var n = 120;
+this[varName] = n;
+console.log(this[varName])
---
+++ user-definedVariableAlt/index.js
@@ -0,0 +1,4 @@
+function createVarName (variable) {
+    variable = document.getElementById('varName').value;
+    document.getElementById('var').innerHTML = variable;
+}
